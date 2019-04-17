c77fa0a71845c0827d4591c1b2ab0c1b0ee4d7ba
LightSun <donshine723@gmail.com>
2019-04-17 23:54:36 +0800

    opt

--- message-protocol/src/main/java/com/heaven7/java/message/protocol/SegmentationPolicy.java
+++ message-protocol/src/main/java/com/heaven7/java/message/protocol/SegmentationPolicy.java
@@ -1,13 +1,15 @@
 package com.heaven7.java.message.protocol;
 
 /**
- * the segmentation policy
+ * the segmentation policy.
+ * this is used for {@linkplain MessageSecure}.
+ * @see MessageSecureWrapper
  * @author heaven7
  */
 public interface SegmentationPolicy {
 
     /**
-     * get the segment length, of every segment
+     * get the secure segment length of every segment. often return the max length.
      * @return the length of segment
      */
     int getSecureSegmentLength();
